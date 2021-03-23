ba8cb2ec2afa12c8a720400264743bd264dd0a36
jiguo.djg <jiguo.djg@dtwave-inc.com>
2021-03-23 21:56:51 +0800

    LRU

--- java/src/main/java/com/djg/algorithm/LRUCache.java
+++ java/src/main/java/com/djg/algorithm/LRUCache.java
@@ -9,9 +9,9 @@ import java.util.LinkedList;
  * @Date 10:44 2021-03-09
  * @Description
  *
- *
- * 技巧：hashmap + 双端队列
- * 其中，hashmap通过key指向双端队列中的node，加快get查询速度。可以看作双写问题，每一步操作双端队列，都要考虑map
+ * 核心思想：
+ * 1. 将最近使用过的node取出，并从队头重新插入
+ * 2. 利用hashmap + 双端队列，其中，hashmap通过key指向双端队列中的node，加快get查询速度。可以看作双写问题，每一步操作双端队列，都要考虑map
  *
  * https://github.com/jiajunhua/labuladong-fucking-algorithm/blob/master/%E9%AB%98%E9%A2%91%E9%9D%A2%E8%AF%95%E7%B3%BB%E5%88%97/LRU%E7%AE%97%E6%B3%95.md
  *
@@ -44,7 +44,7 @@ public class LRUCache {
         if (!map.containsKey(key))
             return -1;
         int val = map.get(key).val;
-        // 利用 put 方法把该数据提前
+        // 最新使用的值，提到队头
         put(key, val);
         return val;
     }
@@ -53,13 +53,13 @@ public class LRUCache {
         // 先把新节点 x 做出来
         Node x = new Node(key, val);
 
+        // 如果队列中存在该值，将值取出，放到队头，并更新map中key对应的值
         if (map.containsKey(key)) {
-            // 删除旧的节点，新的插到头部
             cache.remove(map.get(key));
             cache.addFirst(x);
-            // 更新 map 中对应的数据
             map.put(key, x);
         } else {
+            // 如果队列中不存在该值，判断队列是否已满，满了，就移除队尾值，将新值插入队头；未满，直接将新值插入队头；最后，更新map中key对应的值
             if (cap == cache.size()) {
                 // 删除链表最后一个数据
                 Node last = (Node) cache.removeLast();
